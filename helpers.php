4940853f036d0c1677f0f8cf49541c497381c451
Fernando Gutierrez <fernando@beeb.mx>
2021-08-06 10:34:53 -0500

    Remove bundle dependencies

--- helpers.php
+++ helpers.php
@@ -5,10 +5,7 @@ use Illuminate\Contracts\Support\Htmlable;
 if (!function_exists('b')) {
     function b($value, $doubleEncode = true)
     {
-        if ($value instanceof Htmlable) {
-            return $value->toHtml();
-        }
-        return htmlspecialchars($value, ENT_QUOTES, 'UTF-8', $doubleEncode);
+        return _e($value, $doubleEncode);
     }
 }
 
